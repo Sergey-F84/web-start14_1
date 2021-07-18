67cd2a0d4d34c98810928da1dd94a7a0d52ee351
Sergey-F84 <legal77@bk.ru>
2021-07-18 20:39:21 +0900

    create: addet paralax effect

--- js/main.js
+++ js/main.js
@@ -1,19 +1,37 @@
-var mySwiper = new Swiper('.swiper-container', {
+var hotelslider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
+  },
+});
+var reviewsslider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
   },
 });
 $(document).ready(function() {
   $(document).keydown(function(e){
   if (e.which == 37) {
-     $('.slider-button--prev')[0].click();
+     $('.hotel-slider__button--prev')[0].click();
     };  
   if (e.which == 39) { 
-     $('.slider-button--next')[0].click();
+     $('.hotel-slider__button--next')[0].click();
     };      
  })
   });
+$(window).scroll(function(){
+  $('.newsletter').bgscroll({
+    direction: 'bottom', // направление bottom или top
+    bgpositionx: 50, // x позиция фонового изображения, от 0 до 100, размерность в %, 50 - означает по центру
+    debug: false, // Режим отладки
+    min:0, // минимальное положение (в %) на которое может смещаться фон
+    max:100 // максимальное положение (в %) на которое может смещаться фон
+  });
+})
